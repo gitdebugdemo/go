e35cd3d32fe0772f780daa61878a8dd8ddf2be17
xltxlm <xltxlm@qq.com>
2019-12-24 15:12:19 +0800

    注释

--- CMD.sh
+++ CMD.sh
@@ -3,6 +3,6 @@ cd `dirname $0`
 #输出错误和成功信息的函数,带底色
 function error(){ echo  -e "\033[41;36m $1 \033[0m";}
 function ok(){ echo  -e "\033[42;30m $1 \033[0m";}
-./before.sh
+source ./before.sh
 ./main.sh
 ./after.sh

dd91e6d1222a64b52c5a4c4d50215764d77c4572
xltxlm <xltxlm@qq.com>
2019-12-24 18:25:59 +0800

    注释

--- test/http_test.go
+++ test/http_test.go
@@ -17,3 +17,14 @@ func TestPOSTjson(t *testing.T) {
 	body, _ := ioutil.ReadAll(resp.Body)
 	assert.Equal(t, string(body), bodys)
 }
+
+//模拟post - 提交json整段数据
+func TestPOSTjson2(t *testing.T) {
+	var bodys string
+	bodys = `{"xx":"2不知道"}`
+	resp, _ := http.Post("http://127.0.0.1:8080/json", "application/json;charset=utf-8", bytes.NewBuffer([]byte(bodys)))
+	//程序在使用完回复后必须关闭回复的主体。
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, string(body), bodys)
+}

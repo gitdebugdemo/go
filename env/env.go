9997d1ae6e60ec147f1950821cab62b3a0a3f606
xltxlm <xltxlm@qq.com>
2019-12-30 18:38:06 +0800

    注释

--- env/env.go
+++ env/env.go
@@ -13,8 +13,8 @@ var once sync.Once
 //NewEnvinfo 获取当前编译的环境配置
 func NewEnvinfo() *Envinfo {
 	once.Do(func() {
-		//m = &Envinfo{Dockername: "demo"}
-		m = &Envinfo{Dockername: "dev_go_demo"}
+		m = &Envinfo{Dockername: "demo"}
+		//m = &Envinfo{Dockername: "dev_go_demo"}
 	})
 	return m
 }
